3bcc43557bde10d015239e08c8bc535c2132b11d
Don <pullend15@gmail.com>
2022-11-11 12:16:31 -0500

    finalizing the section

--- calculator.ts
+++ calculator.ts
@@ -6,27 +6,26 @@ interface calculatedResult {
     targetReached: boolean,
     Rating: number,
     explanation: string,
+    target: number
 }
 
-let days : number[] = [3, 0, 2, 4.5, 0, 3, 1]
 
-const exercise = () : calculatedResult => {
+const exercise = (week : string[], target:number) : calculatedResult => {
     let total = 0
     let days = 0 
     let success = false
     let description = " "
     let rating = 0 
-    let splitList =  process.argv.slice(2, 12)
-    for(let i = 0; i < splitList.length;i++){
-        if (Number(splitList[i]) > 0) {
+    for(let i = 0; i < week.length;i++){
+        if (Number(week[i]) > 0) {
             days = days + 1
-            total = total + Number(splitList[i]) 
+            total = total + Number(week[i]) 
         }else{
             total = total + 0 
         }
     }
 
-    let average = total / Number(splitList.length) 
+    let average = total / Number(week.length) 
 
     if(average > 2 ){
         success = true 
@@ -51,9 +50,9 @@ const exercise = () : calculatedResult => {
         calculatedAverage: average,
         targetReached: success,
         Rating: rating,
-        explanation: description
+        explanation: description,
+        target: Number(target)
     }
 }
 
-
-console.log(exercise())
+export default exercise
--- index.ts
+++ index.ts
@@ -1,20 +1,43 @@
 import express from 'express'
 import calculateBMI from './calculateBMI'
+import exercise from './calculator'
 
 const app = express()
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get("/", (_req, res) => {
     res.send("something")
 })
 
 app.get("/bmi/:height/:weight", (_req, res) => {
+    const {height, weight} = _req.params
+    try{
+        res.json({
+            bmi: calculateBMI(Number(height), Number(weight)),
+            height: height,
+            weight: weight
+        })
+    }
+    catch{
+        res.json({
+            error: "malformatted parameters"
+        })
+    }
+})
 
-    let {height, weight} = _req.params
-    res.json({
-        bmi: calculateBMI(Number(height), Number(weight)),
-        height: height,
-        weight: weight
-    })
+app.post("/exercise", (req, res) => {
+    console.log(req.body)
+    const {daily_exercises, target} = req.body
+    try{
+        res.json(exercise(daily_exercises, target))
+    }
+    catch{
+        res.json({
+            error: "there was an error on the page"
+        })
+    }
+
 })
 
 const PORT = 3001
